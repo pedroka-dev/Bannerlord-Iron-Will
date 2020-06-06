918cda984c1c2b79ba106777f5e46ea0b09d8ca2
Pedro Henric Rosário <50923316+pedro-ca@users.noreply.github.com>
2020-06-06 15:15:00 -0300

    Wound Experience initial commit

---
+++ WoundXP/TroopRosterPatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using HarmonyLib;
+using Helpers;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using xxWoundXP;
+
+namespace xWoundXP
+{
+    [HarmonyPatch(typeof(TroopRoster), "WoundTroop")]
+    public class TroopRosterPatch
+    {
+        static void Postfix(TroopRoster __instance, CharacterObject troop, int numberToWound, UniqueTroopDescriptor troopSeed)
+         {
+            if (troop.IsHero)
+            {
+                int xpValue = WoundXpSubModule.heroWoundXpValue;
+
+                Hero heroTroop = troop.HeroObject;
+                heroTroop.AddSkillXp(DefaultSkills.Athletics, xpValue);
+
+                if (troop.IsPlayerCharacter || heroTroop.IsPlayerCompanion || WoundXpSubModule.debugInfo)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage(heroTroop.Name + "received " + xpValue + " Athletics XP for survinving after being wounded."));
+                }
+            }
+            else
+            {
+                int xpValue = WoundXpSubModule.troopWoundXpValue;
+
+                __instance.AddXpToTroop(xpValue, troop);
+
+                if(WoundXpSubModule.debugInfo)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage(troop.Name + " received " + xpValue + " XP for survinving after being wounded."));
+                }
+            }
+        }
+    }
+}
+
---
+++ WoundXP/WoundXpSubModule.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+using TaleWorlds.Core;
+using TaleWorlds.GauntletUI.PrefabSystem;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace xxWoundXP
+{
+    public class WoundXpSubModule : MBSubModuleBase
+    {
+        public static int troopWoundXpValue = 20;
+        public static int heroWoundXpValue = 20;
+        public static bool debugInfo = false;
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+            //Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
+            //     new TextObject("Message", null), 9990,
+            //     () => { InformationManager.DisplayMessage(new InformationMessage("WoundXp loaded sucessfully.")); },
+            //false));
+            
+            try
+            {
+                Harmony harmony = new Harmony("mod.bannerlord.woundxp");
+                harmony.PatchAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Initialising Bannerlord Tweaks:\n\n" + ex.Message);
+            }
+
+            //InformationManager.DisplayMessage(new InformationMessage("WoundXp loaded sucessfully."));
+        }
+    }
+}
