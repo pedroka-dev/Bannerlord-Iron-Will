53e0c93445a395ca59acfb28869f558f57750106
pedro-ca <pedro.henric.rosario@gmail.com>
2020-06-18 19:09:34 -0300

    Serialize and Deserialize for xml

--- WoundXP/TroopRosterPatch.cs
+++ WoundXP/TroopRosterPatch.cs
@@ -11,7 +11,7 @@ namespace xWoundXP
     public class TroopRosterPatch
     {
         static void Postfix(TroopRoster __instance, CharacterObject troop, int numberToWound, UniqueTroopDescriptor troopSeed)
-         {
+        {
             try
             {
                 if (troop.IsHero)
@@ -23,7 +23,7 @@ namespace xWoundXP
 
                     if (troop.IsPlayerCharacter || heroTroop.IsPlayerCompanion || WoundXpSubModule.settings.debugInfo)
                     {
-                        InformationManager.DisplayMessage(new InformationMessage(heroTroop.Name + " received " + xpValue + " Athletics XP for survinving after being wounded.",Colors.Yellow));
+                        InformationManager.DisplayMessage(new InformationMessage(heroTroop.Name + " received " + xpValue + " Athletics XP for surviving after being wounded.", Colors.Yellow));
                         WoundXpSubModule.Log.Info("Hero Troop: " + troopSeed.ToString() + " | " + heroTroop.Name + " received Athletics XP value of " + xpValue);
                     }
                 }
@@ -35,14 +35,14 @@ namespace xWoundXP
 
                     if (WoundXpSubModule.settings.debugInfo)
                     {
-                        InformationManager.DisplayMessage(new InformationMessage(troop.Name + " received " + xpValue + " XP for survinving after being wounded.", Colors.Yellow));
+                        InformationManager.DisplayMessage(new InformationMessage(troop.Name + " received " + xpValue + " XP for surviving after being wounded.", Colors.Yellow));
                         WoundXpSubModule.Log.Info("Generic Troop: " + troopSeed.ToString() + " | " + troop.Name + " received XP value of " + xpValue);
                     }
                 }
             }
             catch (Exception ex)
             {
-                InformationManager.DisplayMessage(new InformationMessage("Something went wrong with Iron Will - Wound Experience. Please check the log file at \\bin\\Win64_Shipping_Client\\: " + WoundXpSubModule.LogFilePath, Colors.Red)); 
+                InformationManager.DisplayMessage(new InformationMessage("Something went wrong with Iron Will - Wound Experience. Please check the log file at \\bin\\Win64_Shipping_Client\\: " + WoundXpSubModule.settings.LogFilePath, Colors.Red)); 
                 WoundXpSubModule.Log.Error(ex, "Error on Harmony Patch for WoundTroop.");
             }
         }
--- WoundXP/WoundXpSubModule.cs
+++ WoundXP/WoundXpSubModule.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
+using System.Xml.Serialization;
 using HarmonyLib;
 using NLog;
 using NLog.Config;
@@ -16,31 +18,50 @@ namespace xxWoundXP
 {
     public class WoundXpSubModule : MBSubModuleBase
     {
+        public static readonly NLog.Logger Log = LogManager.GetCurrentClassLogger();
+        public static ModuleSettings settings;
+
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
 
+            settings = new ModuleSettings();
+
             LoggingConfiguration loggingConfiguration = new LoggingConfiguration();
-            FileTarget target = new FileTarget(LogFileTarget)
+            FileTarget target = new FileTarget(settings.LogFileTarget)
             {
-                FileName = LogFilePath
+                FileName = settings.LogFilePath
             };
             loggingConfiguration.AddRule(LogLevel.Debug, LogLevel.Fatal, target, "*");
             LogManager.Configuration = loggingConfiguration;
 
             try
             {
+                if (!File.Exists(@"\"+ settings.SettingsFilePath))
+                {
+                    SerializeSettings(settings.SettingsFilePath);
+                }
+
+                settings = DeserializeSettings(settings.SettingsFilePath);
+                Log.Info("Module intialization | Settings initialized sucessfully.");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Failed to Serialize/Deserialize for " + settings.SettingsFilePath); ;
+            }
+
+            try
+            {
                 Harmony harmony = new Harmony("mod.bannerlord.woundxp");
                 harmony.PatchAll();
+
                 Log.Info("Module intialization | Harmony Initialized sucessfully.");
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error Initialising Iron Will - Wound Experience:\n\n" + ex.Message);
-                Log.Error(ex, "Failed to initialize Harmony.");
+                
             }
-
-            
         }
 
         protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
@@ -54,12 +75,24 @@ namespace xxWoundXP
             Log.Info("Module intialization | Campaing Game Initialized.");
         }
 
-        public static readonly string LogFileTarget = "WoundExperienceLogFile";
-        public static readonly string LogFilePath = "WoundExperienceLog.txt";
-        
-        public static readonly NLog.Logger Log = LogManager.GetCurrentClassLogger();
 
-        public static ModuleSettings settings = new ModuleSettings();
+        public void SerializeSettings(string fileName)  //Serializes default values
+        {
+            XmlSerializer s = new XmlSerializer(typeof(ModuleSettings));
+            TextWriter writer = new StreamWriter(fileName);
+
+            s.Serialize(writer, settings);
+            writer.Close();
+        }
+
+        public ModuleSettings DeserializeSettings(string fileName) //Deserialize from file
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            XmlSerializer x = new XmlSerializer(typeof(ModuleSettings));
+            ModuleSettings ms = (ModuleSettings)x.Deserialize(fs);
+
+            return ms;
+        }
     }
 
 }
